3e8180f0e58c93370a2676750b940b0a0080dd45
João Pedro <jplmarotinho22@gmail.com>
2025-10-27 13:36:06 -0300

    Domain Pet preenchido

--- src/main/java/br/com/alura/domain/Pet.java
+++ src/main/java/br/com/alura/domain/Pet.java
@@ -1,4 +1,26 @@
 package br.com.alura.domain;
 
 public class Pet {
+    public Pet(
+            String nome,
+            String tipo,
+            String raca,
+            int idade,
+            String cor,
+            Float peso
+    ) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.cor = cor;
+        this.idade = idade;
+        this.peso = peso;
+        this.raca = raca;
+    }
+
+    private String nome;
+    private String tipo;
+    private String raca;
+    private int idade;
+    private String cor;
+    private Float peso;
 }
