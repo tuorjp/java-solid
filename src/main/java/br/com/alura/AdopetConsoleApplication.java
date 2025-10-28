97e19ec0517aee80a4d695d6e01de66e751beacb
João Pedro <jplmarotinho22@gmail.com>
2025-10-28 09:54:50 -0300

    Limpeza do código

--- src/main/java/br/com/alura/AdopetConsoleApplication.java
+++ src/main/java/br/com/alura/AdopetConsoleApplication.java
@@ -1,12 +1,5 @@
 package br.com.alura;
 
-import com.google.gson.JsonObject;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class AdopetConsoleApplication {
