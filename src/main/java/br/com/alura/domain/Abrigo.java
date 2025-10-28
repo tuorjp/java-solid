3b7a94e93472440faebf5452508867d111fba99c
João Pedro <jplmarotinho22@gmail.com>
2025-10-28 08:37:53 -0300

    Construtores, Getters e Setters

--- src/main/java/br/com/alura/domain/Abrigo.java
+++ src/main/java/br/com/alura/domain/Abrigo.java
@@ -1,6 +1,11 @@
 package br.com.alura.domain;
 
 public class Abrigo {
+
+    public Abrigo() {
+
+    }
+
     public Abrigo(String nome, String telefone, String email) {
         this.nome = nome;
         this.telefone = telefone;
@@ -11,12 +16,25 @@ public class Abrigo {
     private String nome;
     private String telefone;
     private String email;
+    private Pet[] pets;
+
+    public Long getId() {
+        return id;
+    }
 
     public String getNome() {
         return nome;
     }
 
-    public Long getId() {
-        return id;
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Pet[] getPets() {
+        return pets;
     }
 }
